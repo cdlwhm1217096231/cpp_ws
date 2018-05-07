7a41ffcf2ca40e7cf4c5329bb767f6496eb41c95
cdlwhm1217096231 <1217096231@qq.com>
2018-05-07 22:03:00 +0800

    c++变量类型

---
+++ demo2.cpp
@@ -0,0 +1,75 @@
+#include<iostream>  
+#include<string>  
+#include <limits>  
+using namespace std;
+
+int main()
+{
+	cout << "type: \t\t" << "************size**************" << endl;
+	cout << "bool: \t\t" << "所占字节数：" << sizeof(bool);
+	cout << "\t最大值：" << (numeric_limits<bool>::max)();
+	cout << "\t\t最小值：" << (numeric_limits<bool>::min)() << endl;
+	cout << "char: \t\t" << "所占字节数：" << sizeof(char);
+	cout << "\t最大值：" << (numeric_limits<char>::max)();
+	cout << "\t\t最小值：" << (numeric_limits<char>::min)() << endl;
+	cout << "signed char: \t" << "所占字节数：" << sizeof(signed char);
+	cout << "\t最大值：" << (numeric_limits<signed char>::max)();
+	cout << "\t\t最小值：" << (numeric_limits<signed char>::min)() << endl;
+	cout << "unsigned char: \t" << "所占字节数：" << sizeof(unsigned char);
+	cout << "\t最大值：" << (numeric_limits<unsigned char>::max)();
+	cout << "\t\t最小值：" << (numeric_limits<unsigned char>::min)() << endl;
+	cout << "wchar_t: \t" << "所占字节数：" << sizeof(wchar_t);
+	cout << "\t最大值：" << (numeric_limits<wchar_t>::max)();
+	cout << "\t\t最小值：" << (numeric_limits<wchar_t>::min)() << endl;
+	cout << "short: \t\t" << "所占字节数：" << sizeof(short);
+	cout << "\t最大值：" << (numeric_limits<short>::max)();
+	cout << "\t\t最小值：" << (numeric_limits<short>::min)() << endl;
+	cout << "int: \t\t" << "所占字节数：" << sizeof(int);
+	cout << "\t最大值：" << (numeric_limits<int>::max)();
+	cout << "\t最小值：" << (numeric_limits<int>::min)() << endl;
+	cout << "unsigned: \t" << "所占字节数：" << sizeof(unsigned);
+	cout << "\t最大值：" << (numeric_limits<unsigned>::max)();
+	cout << "\t最小值：" << (numeric_limits<unsigned>::min)() << endl;
+	cout << "long: \t\t" << "所占字节数：" << sizeof(long);
+	cout << "\t最大值：" << (numeric_limits<long>::max)();
+	cout << "\t最小值：" << (numeric_limits<long>::min)() << endl;
+	cout << "unsigned long: \t" << "所占字节数：" << sizeof(unsigned long);
+	cout << "\t最大值：" << (numeric_limits<unsigned long>::max)();
+	cout << "\t最小值：" << (numeric_limits<unsigned long>::min)() << endl;
+	cout << "double: \t" << "所占字节数：" << sizeof(double);
+	cout << "\t最大值：" << (numeric_limits<double>::max)();
+	cout << "\t最小值：" << (numeric_limits<double>::min)() << endl;
+	cout << "long double: \t" << "所占字节数：" << sizeof(long double);
+	cout << "\t最大值：" << (numeric_limits<long double>::max)();
+	cout << "\t最小值：" << (numeric_limits<long double>::min)() << endl;
+	cout << "float: \t\t" << "所占字节数：" << sizeof(float);
+	cout << "\t最大值：" << (numeric_limits<float>::max)();
+	cout << "\t最小值：" << (numeric_limits<float>::min)() << endl;
+	cout << "size_t: \t" << "所占字节数：" << sizeof(size_t);
+	cout << "\t最大值：" << (numeric_limits<size_t>::max)();
+	cout << "\t最小值：" << (numeric_limits<size_t>::min)() << endl;
+	cout << "string: \t" << "所占字节数：" << sizeof(string) << endl;
+	// << "\t最大值：" << (numeric_limits<string>::max)() << "\t最小值：" << (numeric_limits<string>::min)() << endl;  
+	cout << "type: \t\t" << "************size**************" << endl;
+	return 0;
+}
+// 七种基本的 C++ 数据类型 char int flaot double bool void wchar_t
+// 一些基本类型可以使用一个或多个类型修饰符进行修饰：
+/*
+signed unsigned short long
+*/
+//可以使用 typedef 为一个已有的类型取一个新的名字.如：typedef int feet; feet distance;
+/*
+枚举类型:
+enum 枚举名{
+标识符[=整型常数],
+标识符[=整型常数],
+...
+标识符[=整型常数]
+} 枚举变量;
+如：
+enum color { red, green=5, blue } c;
+c = blue;
+默认情况下，第一个名称的值为 0，第二个名称的值为 1，第三个名称的值为 2，以此类推
+在这里，blue 的值为 6，因为默认情况下，每个名称都会比它前面一个名称大 1，但 red 的值依然为 0
+*/
