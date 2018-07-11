7c6999a0c7625e1139324a44ffda62fe31ab973e
cdlwhm1217096231 <1217096231@qq.com>
2018-07-11 20:44:56 +0800

    c++程序设计---清华大学

---
+++ Tsinghua_demo1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "The size of an int is: " << sizeof(int) << " bytes.\n";
+	cout << "The size of an double is: " << sizeof(double) << " bytes.\n";
+	cout << "The size of an short is: " << sizeof(short) << " bytes.\n";
+	cout << "The size of an char is: " << sizeof(char) << " bytes.\n";
+	cout << "The size of an float is: " << sizeof(float) << " bytes.\n";
+
+	return 0;
+}
