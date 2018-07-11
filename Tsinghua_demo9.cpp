f3428b31f89b598b05c33fbe8d5ce58aa11159db
cdlwhm1217096231 <1217096231@qq.com>
2018-07-11 20:47:48 +0800

    c++程序设计---清华大学

---
+++ Tsinghua_demo9.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+bool symm(unsigned n);
+
+int main()
+{
+	for (unsigned m = 11; m<1000; m++)
+		if (symm(m) && symm(m*m) && symm(m*m*m))
+		{
+			cout << " m=" << m;
+			cout << " m*m=" << m*m;
+			cout << " m*m*m" << m*m*m << endl;
+		}
+	return 0;
+}
+
+// 判断是否是回文数 11 121 1331 的函数
+bool symm(unsigned n)
+{
+	unsigned i = n;
+	unsigned m = 0;
+	while (i>0)
+	{
+		m = m * 10 + i % 10;
+		i = i / 10;
+	}
+	return m == n;
+}
