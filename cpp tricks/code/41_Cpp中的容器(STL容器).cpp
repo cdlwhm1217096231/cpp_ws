e30e8fbe23021783768ebe8ac1b435fb8714f2ce
cdl <1217096231@qq.com>
2019-04-09 09:59:17 +0800

    Cpp中的容器(STL容器)

---
+++ cpp tricks/code/41_Cpp中的容器(STL容器).cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+#include "vector"
+
+
+using namespace std;
+
+
+int main(){
+    // vector<int>   容器类的名字
+    vector<int> a;  // 容器对象a，a代表一个可变长的数组，数组中的每个元素都是int类型的变量
+    vector<double> b; // 容器对象b，b代表一个可变长的数组，数组中的每个元素都是double类型的变量
+    return 0;
+}
